3499c373853f5d14eba1d8fe71e2b466970570f8
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-07-10 15:16:37 -0400

    added buttons

--- src/Review.jsx
+++ src/Review.jsx
@@ -1,10 +1,41 @@
 import { useState } from "react";
 import people from "./Data";
-import { FaChewronLeftFaChewronRight, FaQuoteRight } from "react-icons/fa";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 function Review() {
   const [index, setIndex] = useState(0);
-  const [name, job, image, text] = people[index];
+  const { name, job, image, text } = people[index];
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+
+  const nextBtn = () => {
+    setIndex((index) => {
+      const newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const prevBtn = () => {
+    setIndex((index) => {
+      const newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const surpriseBtn = () => {
+    const newIndex = Math.floor(Math.random() * people.length);
+
+    setIndex(checkNumber(newIndex));
+  };
+
   return (
     <>
       <article className="review">
@@ -17,6 +48,17 @@ function Review() {
         <h4 className="auther">{name}</h4>
         <p className="joc">{job}</p>
         <p className="text">{text}</p>
+        <div className="btn-container">
+          <button onClick={nextBtn} className="next-btn">
+            <FaChevronRight />
+          </button>
+          <button onClick={prevBtn} className="prev-btn">
+            <FaChevronLeft />
+          </button>
+          <button onClick={surpriseBtn} className="next-btn">
+            suprise me
+          </button>
+        </div>
       </article>
       <h2> our reviews </h2>
     </>
