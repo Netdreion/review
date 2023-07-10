59cc12d91faf9f2841270a6fd779efa04a3fed43
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-07-10 13:36:22 -0400

    App component set-up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import { useState } from "react";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <section className="container">
+        <div className="title">
+          <h2> our reviews </h2>
+          <div className="underline "></div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default App;
